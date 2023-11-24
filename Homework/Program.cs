19ae3dc27fcfbba6f7b7f28f8a41e8bd0a57f553
Kristina Shabovich <Kristina.beloshapko@mail,ru>
2023-11-24 21:34:04 +0300

    add Homework

---
+++ Homework/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 1
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+if ((a % 7 == 0) && (a % 23 == 0))
+    Console.WriteLine("да");
+
+else 
+    Console.WriteLine("нет");
