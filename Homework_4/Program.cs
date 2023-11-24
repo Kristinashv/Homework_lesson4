d576afab5d5fe9586c239f049b0e0ed332373307
Kristina Shabovich <Kristina.beloshapko@mail,ru>
2023-11-24 21:46:42 +0300

    add Homework 4

---
+++ Homework_4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 4
+
+Console.WriteLine("Введите число ");
+    int N;
+    int deg = 0;
+    N = Convert.ToInt32(Console.ReadLine());
+    for (int i = N ; i != 0 ; i = i/10)
+    {
+        deg++;
+    }
+    string str = "";
+    for (int i = 0 ; i < deg ; i++)
+    {
+        double N1 = Math.Floor(N/Math.Pow(10,deg-i-1));
+        str = str + Convert.ToString(N1);
+        if (deg - i != 1)
+        {
+            str = str + ",";
+        }
+        N = Convert.ToInt32(N % Math.Pow(10,deg-i-1));
+    }
+    Console.Write(str);
