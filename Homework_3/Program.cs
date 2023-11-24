bd1c34cbba0815a0f76c510785339b3eeff7a053
Kristina Shabovich <Kristina.beloshapko@mail,ru>
2023-11-24 21:44:26 +0300

    add Homework 3

---
+++ Homework_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 3
+int num;
+    do
+    {
+        Console.Write("Введите число : ");
+        num = Convert.ToInt32(Console.ReadLine());
+        if ((10 > num)||(num > 99))
+        {
+            Console.WriteLine("Число не удовлетворяет условию");
+        }
+    }while((10 > num)||(num > 99));
+int n1 = num / 10;
+int n2 = num % 10;
+if(n1 > n2)
+   Console.WriteLine(n1);
+else
+   Console.WriteLine(n2);
+
