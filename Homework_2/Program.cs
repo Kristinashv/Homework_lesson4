0385a00081676a0301c33f152b9e07b8319b97e0
Kristina Shabovich <Kristina.beloshapko@mail,ru>
2023-11-24 21:41:31 +0300

    add Homework 2

---
+++ Homework_2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 2
+
+int x , y;
+    do
+    {
+        Console.Write("Введите x : ");
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите y : "); 
+        y = Convert.ToInt32(Console.ReadLine());
+        if ((x==0)||(y==0))
+        {
+            Console.WriteLine("Введите целые числа для обеих переменных");
+        }
+    }while((x==0)||(y==0));
+    if ((x>0)&&(y>0))
+    {
+      Console.Write("1 четверть");
+    }
+    if ((x<0)&&(y>0))
+    {
+      Console.Write("2 четверть");
+    }
+    if ((x<0)&&(y<0))
+    {
+      Console.Write("3 четверть");
+    }
+    if ((x>0)&&(y<0))
+    {
+      Console.Write("4 четверть");
+    }
+
