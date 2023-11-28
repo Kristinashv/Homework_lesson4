dd5a45af6d08e374a85603683eb9258128f3cc24
Kristina Shabovich <Kristina.beloshapko@mail,ru>
2023-11-29 00:36:36 +0300

    add Home work 1

---
+++ Home.work_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 1
+
+int[] array = new int[10];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(100);
+    Console.Write(array[i]+" ");
+}
+Console.WriteLine();
+int n = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if((20 < array[i]) && (array[i] < 90))
+    {
+        n++;
+    }
+}
+Console.WriteLine("количество = "+n);
