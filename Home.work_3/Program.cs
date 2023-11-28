cba51c28bf354af49c471fd9b23f25b2378770b6
Kristina Shabovich <Kristina.beloshapko@mail,ru>
2023-11-29 01:01:46 +0300

    add Home work 3

---
+++ Home.work_3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 3
+
+double[] array = new double[10];
+    Random rand = new Random();
+    for (int i = 0 ; i < array.Length; i++)
+    {
+        array[i] = rand.NextDouble() * 100;
+        Console.Write(array[i]+" ");
+    }
+    Console.WriteLine();
+    double max = array[0];
+    double min = array[0];
+    for (int i = 0 ; i < array.Length; i++)
+    {
+        if (max < array[i])
+        {
+            max = array[i];
+        }
+        if (min > array[i])
+        {
+            min = array[i];
+        }
+    }
+    double ans = max - min; 
+    Console.WriteLine("Answer = " + ans);
