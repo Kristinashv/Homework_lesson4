4bb324c97ef39321e2a9714948fba97c4e49dc6d
Kristina Shabovich <Kristina.beloshapko@mail,ru>
2023-11-29 00:59:23 +0300

    add Home work 2

---
+++ Home.work_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 2
+
+    Random rand = new Random();
+    int[] array = new int [10];
+    for (int i = 0; i < array.Length ; i++)
+    {
+        array[i] = rand.Next(100);
+        Console.Write(array[i]+" ");
+    }
+    Console.WriteLine();
+    int n = 0;
+    for (int i = 0; i < array.Length ; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            n++;
+        }
+    }
+    Console.Write("количество четныx чисел = " + n);
+    
+
