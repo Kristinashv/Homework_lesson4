b0f0d26f38b223a15c56d9601325df5b23fcae65
Kristina Shabovich <Kristina.beloshapko@mail,ru>
2023-12-03 15:38:25 +0300

    add Zadacha 2

---
+++ Zadacha_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Zadacha 2
+
+int[] arr = new int [10];
+Random rand = new Random();
+Console.Write("Массив случайных чисел : ");
+for(int i = 0; i < arr.Length ; i++)
+{
+    arr[i] = rand.Next(100,1000);
+    Console.Write(arr[i]+" ");
+}
+Console.WriteLine();
+int n = 0;
+for(int i = 0; i < arr.Length ; i++)
+{
+    if (arr[i] % 2 == 0)
+    {
+        n++;
+    }
+}
+Console.WriteLine("Количество четных чисел = " + n);
+
+
