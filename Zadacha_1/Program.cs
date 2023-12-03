6cd50da6b74215f724e2bec9b57898e5e484bb95
Kristina Shabovich <Kristina.beloshapko@mail,ru>
2023-12-03 15:34:52 +0300

    add Zadacha 1

---
+++ Zadacha_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Zadacha 1
+
+string num;  
+    bool key = false;
+    do
+    {
+        Console.Write("Введите двухзначное число : ");
+        num = Console.ReadLine();
+        if (num == "q")
+        {
+            Console.WriteLine("Введен символ q");
+        }
+        int num1 = Int32.Parse(num);
+        int n1 = num1/10;
+        int n2 = num1%10;
+        if ((n2+n1)%2==0)
+        {
+            Console.Write("Введено число сумма цифр которых делиться на 2");
+            key = true;
+        }
+    }while((num == "q")||(key == false));
